90b537a2b28eddad54fee947d48e87a0c678e882
root <root@DESKTOP-T2H6NRG.localdomain>
2025-06-06 16:13:29 -0700

    Set up script, run everything for me

---
+++ server_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash/
+
+if [ ! -f "/root/Project_part_2/my_key" ]; then
+	ssh-keygen -t rsa -b 4096 -f "my_key"
+fi
+
+cd terraform
+terraform init
+terraform apply -auto-approve
+
+ip=$(terraform output -raw instance_public_ip)
+
+cd ../ansible
+
+echo "[minecraft]" > inventory
+echo "${ip} ansible_ssh_user=ec2-user ansible_ssh_private_key_file=$pwd/../my_key" >> inventory
+
+ansilbe-playbook -i inventory playbook.yml
